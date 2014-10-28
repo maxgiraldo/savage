54901d1e2d6eb6b3e27919ce48bae87a82a68679
Max Giraldo <dr.datacode@gmail.com>
2014-10-27 20:20:55 -0700

    Convert SVG img tag into in-line svg and edit fill value of specific shapes

--- src/savage.js
+++ src/savage.js
@@ -27,13 +27,39 @@ function Savage() {
     }
 }
 
-Savage.prototype.edit = function(el) {
-    return {
-        color: function(hex) {
+/**
+    Replace image with SVG code
+**/
 
+Savage.prototype.convert = function(elID) {
+    var $img = $( '#' + elID );
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        var $svg = $(data).find('svg');
+
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        $svg = $svg.removeAttr('xmlns:a');
+
+        $img.replaceWith($svg);
+    }, 'xml');
+}
+
+Savage.prototype.edit = function($shapeID) {
+    var $el = $('#' + $shapeID);
+    return {
+        color: function(color) {
+            $el.removeAttr('style');
+            $el.css('fill', color);
         }
     }
 }
-
-// eye.edit($el)
-//     .color('#000');
--- test/js/app.js
+++ test/js/app.js
@@ -1,2 +1,8 @@
-var sample = document.getElementById('sample');
-console.log(sample)
+var savage = new Savage();
+
+savage.convert('sample');
+
+$('#clicky').click(function(){
+    savage.edit('outer-rect')
+        .color('#fff');
+});
