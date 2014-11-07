5f9ae2846082cb08851ea1429b2733abdd07a192
Max Giraldo <dr.datacode@gmail.com>
2014-11-06 22:48:44 -0800

    create angular directive

--- test/js/app.js
+++ test/js/app.js
@@ -1,6 +1,19 @@
-console.log($('#sample'))
-var img = new Savage('sample');
-img.edit('outer-rect')
-    .fill('#5bff38')
-    .stroke('#ff362a', 8);
-console.log('img', img)
+angular.module('testApp', [])
+    .directive('savage', function() {
+        return {
+            restrict: 'E',
+            link: function(scope, element, attrs) {
+                var img = new Savage(attrs.id);
+                element.on('click', function() {
+                    img.edit('outer-rect').fill('#000')
+                });
+            },
+            template: '<iframe id="sample" src="img/sample.svg"></iframe>'
+        }
+    });
+// console.log($('#sample'))
+// var img = new Savage('sample');
+// img.edit('outer-rect')
+//     .fill('#5bff38')
+//     .stroke('#ff362a', 8);
+// console.log('img', img)
